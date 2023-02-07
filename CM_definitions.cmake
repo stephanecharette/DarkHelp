01d2073acc021f7dfd49bc6691899b645024e306
olokos <1755581+olokos@users.noreply.github.com>
2023-02-07 17:47:36 +0100

    Fix windows builds

--- CM_definitions.cmake
+++ CM_definitions.cmake
@@ -8,6 +8,7 @@ IF (WIN32)
 	ADD_COMPILE_OPTIONS ( /WX )				# treat warnings as errors
 	ADD_COMPILE_OPTIONS ( /permissive- )	# stick to C++ standards (turn off Microsoft-specific extensions)
 	ADD_COMPILE_OPTIONS ( /wd4100 )			# disable "unreferenced formal parameter"
+	ADD_COMPILE_OPTIONS ( /wd4127 )			# disable "conditional expression is constant"
 	ADD_COMPILE_DEFINITIONS ( _CRT_SECURE_NO_WARNINGS )	# don't complain about localtime()
 	SET ( CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>" )
 ELSE ()
