36dbec69a350334e294478c64c84300bc980e7da
olokos <1755581+olokos@users.noreply.github.com>
2023-02-07 18:51:45 +0100

    Windows fix - make scripts more generic

--- build_windows.cmd
+++ build_windows.cmd
@@ -10,14 +10,13 @@ mkdir build
 cd build
 
 set ARCHITECTURE=x64
-rem set BUILD_TYPE=Release
+rem set BUILD_TYPE=Debug
 set BUILD_TYPE=Release
-set VCPKG_PATH=C:\DarknetOpenCV\vcpkg
+set VCPKG_PATH=C:\src\vcpkg
 rem set TRIPLET=x64-windows-static
 set TRIPLET=x64-windows
-set CUDA_BIN_PATH="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.8/bin"
 
-cmake -A %ARCHITECTURE% -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DCMAKE_TOOLCHAIN_FILE=%VCPKG_PATH%/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=%TRIPLET% -DCMAKE_CUDA_COMPILER=%CUDA_BIN_PATH% ..
+cmake -A %ARCHITECTURE% -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DCMAKE_TOOLCHAIN_FILE=%VCPKG_PATH%/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=%TRIPLET% -DCMAKE_CUDA_COMPILER="%CUDA_PATH%" ..
 if ERRORLEVEL 1 goto END
 
 msbuild.exe /property:Platform=%ARCHITECTURE%;Configuration=%BUILD_TYPE% /target:Build -maxCpuCount -verbosity:normal -detailedSummary DarkHelp.sln
