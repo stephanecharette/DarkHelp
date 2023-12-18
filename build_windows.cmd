b26fcb58d3e7a1cb3799966cf77dbdd7a3526078
Stephane Charette <stephanecharette@gmail.com>
2023-12-17 23:52:50 -0800

    update Windows build command

--- build_windows.cmd
+++ build_windows.cmd
@@ -12,18 +12,18 @@ cd build
 set ARCHITECTURE=x64
 rem set BUILD_TYPE=Debug
 set BUILD_TYPE=Release
-set VCPKG_PATH=C:\src\vcpkg
+set VCPKG_PATH=C:/src/vcpkg
 rem set TRIPLET=x64-windows-static
 set TRIPLET=x64-windows
 
-cmake -A %ARCHITECTURE% -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DCMAKE_TOOLCHAIN_FILE=%VCPKG_PATH%/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=%TRIPLET% -DCMAKE_CUDA_COMPILER="%CUDA_PATH%" ..
-if ERRORLEVEL 1 goto END
+cmake -A %ARCHITECTURE% -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DCMAKE_TOOLCHAIN_FILE=%VCPKG_PATH%/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=%TRIPLET% -DDarknet="C:/Program Files/Darknet/lib/darknet.lib" ..
+if %ERRORLEVEL% neq 0 goto END
 
 msbuild.exe /property:Platform=%ARCHITECTURE%;Configuration=%BUILD_TYPE% /target:Build -maxCpuCount -verbosity:normal -detailedSummary DarkHelp.sln
-if ERRORLEVEL 1 goto END
+if %ERRORLEVEL% neq 0 goto END
 
 msbuild.exe /property:Platform=%ARCHITECTURE%;Configuration=%BUILD_TYPE% PACKAGE.vcxproj
-if ERRORLEVEL 1 goto END
+if %ERRORLEVEL% neq 0 goto END
 
 :END
 cd ..
